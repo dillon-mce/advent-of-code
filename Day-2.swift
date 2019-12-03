14aaeb12699ce4bd4fbd647d3293845acc103558
Dillon McElhinney <dillon.mce@gmail.com>
2019-12-02 20:26:54 -0700

    Update template and day 2 to make Computer more extensible

--- Day-2.swift
+++ Day-2.swift
@@ -1,6 +1,7 @@
 #!/usr/bin/swift
 import Cocoa
 
+// MARK: Input Handling
 let input = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : ""
 var day = "DAY 2"
 day += input == "" ? " – TEST" : ""
@@ -8,55 +9,91 @@ let underscores = Array(repeating: "—", count: day.count).joined()
 
 print("\n\(underscores)\n\(day)\n\(underscores)")
 
-// Tests
+// MARK: Custom Types
+class Computer {
+    enum Operation {
+        case binary((Int, Int, Int) -> Void)
+        case unary((Int, Int) -> Void)
+        case exit
+
+        func increment() -> Int {
+            switch self {
+            case .binary:
+                return 4
+            case .unary:
+                return 3
+            default:
+                return 1
+            }
+        }
+    }
+
+    private func add(_ a: Int, _ b: Int, c: Int) {
+        memory[c] = memory[a] + memory[b]
+    }
+    private func mul(_ a: Int, _ b: Int, c: Int) {
+        memory[c] = memory[a] * memory[b]
+    }
+
+    lazy var operations: [Int: Operation] = [
+        1: .binary(add),
+        2: .binary(mul),
+        99: .exit
+    ]
+
+    var storage: [Int] = []
+    private var memory: [Int] = []
+
+    func run() -> [Int] {
+        memory = storage
+        var pointer = 0
+        while true {
+            let opCode = memory[pointer]
+            guard let op = operations[opCode] else {
+                return memory
+            }
+            switch op {
+            case .binary(let handler):
+                let a = memory[pointer+1]
+                let b = memory[pointer+2]
+                let c = memory[pointer+3]
+                handler(a, b, c)
+                pointer += op.increment()
+            case .unary:
+                break
+            case .exit:
+                return memory
+            }
+        }
+    }
+}
+
+// MARK: Tests
 let test1 = "1,9,10,3,2,3,11,0,99,30,40,50"
 let test2 = "1,1,1,4,99,5,6,0,99"
 
-// Part 1
+// MARK: Part 1
 func solvePart1(_ string: String) -> Int {
-    let array = string.components(separatedBy: ",").compactMap { Int($0) }
-    let result = runProgram(array)
+    let computer = Computer()
+    computer.storage = string.components(separatedBy: ",").compactMap { Int($0) }
+    let result = computer.run()
     return result[0]
-}
-
-func runProgram(_ array: [Int]) -> [Int] {
-    var result = array
-    var pointer = 0
-    while true {
-        switch result[pointer] {
-        case 1:
-            let a = result[pointer+1]
-            let b = result[pointer+2]
-            let c = result[pointer+3]
-            result[c] = result[a] + result[b]
-            pointer += 4
-        case 2:
-            let a = result[pointer+1]
-            let b = result[pointer+2]
-            let c = result[pointer+3]
-            result[c] = result[a] * result[b]
-            pointer += 4
-        case 99:
-            return result
-        default:
-            break
-        }
-    }
 }
 
 //print(solvePart1(test1))
 assert(solvePart1(test1) == 3500)
 assert(solvePart1(test2) == 30)
 
-// Part 2
+// MARK: Part 2
 func solvePart2(_ string: String) -> Int {
-    var array = string.components(separatedBy: ",").compactMap { Int($0) }
     let target = 19690720
+    let computer = Computer()
+    computer.storage = string.components(separatedBy: ",").compactMap { Int($0) }
     for noun in 0...99 {
         for verb in 0...99 {
-            array[1] = noun
-            array[2] = verb
-            let result = runProgram(array)
+            computer.storage[1] = noun
+            computer.storage[2] = verb
+            let result = computer.run()
             if result[0] == target {
                 return 100 * noun + verb
             }
@@ -67,6 +104,7 @@ func solvePart2(_ string: String) -> Int {
 
 //assert(solvePart2(test2) == "")
 
+// MARK: Execution
 func findAnswers(_ string: String) {
     var string = string
     if string.isEmpty { string = test1 }
--- _template.swift
+++ _template.swift
@@ -1,6 +1,7 @@
 #!/usr/bin/swift
 import Cocoa
 
+// MARK: Input Handling
 let input = CommandLine.arguments.count > 1 ? CommandLine.arguments[1] : ""
 var day = "DAY "
 day += input == "" ? " – TEST" : ""
@@ -8,11 +9,11 @@ let underscores = Array(repeating: "—", count: day.count).joined()
 
 print("\n\(underscores)\n\(day)\n\(underscores)")
 
-// Tests
+// MARK: Tests
 let test1 = ""
 let test2 = ""
 
-// Part 1
+// MARK: Part 1
 func solvePart1(_ string: String) -> String {
 
     return "Answer part 1 here"
@@ -21,7 +22,7 @@ func solvePart1(_ string: String) -> String {
 //print(solvePart1(test1))
 //assert(solvePart1(test1) == 12)
 
-// Part 2
+// MARK: Part 2
 func solvePart2(_ string: String) -> String {
 
     return "Answer part 2 here"
@@ -30,6 +31,7 @@ func solvePart2(_ string: String) -> String {
 //print(solvePart2(test2))
 //assert(solvePart2(test2) == "")
 
+// MARK: Execution
 func findAnswers(_ string: String) {
     var string = string
     if string.isEmpty { string = test1 }
